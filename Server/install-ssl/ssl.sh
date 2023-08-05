d2e0b5ff30fbfb620932ad1561c0b7ffe7177570
muhammadhashemi <muhammad.h.0106@gmail.com>
2023-08-05 21:02:27 +0000

    Added a New Domain

--- Server/install-ssl/ssl.sh
+++ Server/install-ssl/ssl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-certbot --nginx -d minio.shomamamama.com -d minioapi.shomamamama.com -d taco.shomamamama.com --non-interactive --agree-tos -m muhammad.h.0106@gmail.com
+certbot --nginx -d harbor.shomamamama.com -d minio.shomamamama.com -d minioapi.shomamamama.com -d taco.shomamamama.com --non-interactive --agree-tos -m muhammad.h.0106@gmail.com
