f0106180015b5b0b1bffe04a65fca9aa9f5006f1
muhammadhashemi <muhammad.h.0106@gmail.com>
2023-08-14 07:44:05 +0000

    Updated

--- Server/install-ssl/ssl.sh
+++ Server/install-ssl/ssl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-certbot --nginx -d minio.shomamamama.com -d minioapi.shomamamama.com -d taco.shomamamama.com --non-interactive --agree-tos -m muhammad.h.0106@gmail.com
+certbot --nginx -d postgres.shomamamama.com -d redis.shomamamama.com -d minio.shomamamama.com -d minioapi.shomamamama.com -d taco.shomamamama.com --non-interactive --agree-tos -m muhammad.h.0106@gmail.com
