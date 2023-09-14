f7b5cbe8ffd72474ac71a97bd67b85d01aeefb98
hu-mamad <muhammad.h.0106@gmail.com>
2023-09-14 04:13:52 +0330

    Update

--- Taco/rmi.sh
+++ Taco/rmi.sh
@@ -2,9 +2,4 @@
 
 image="harbor.shomamamama.com/hohohoo/taco:latest"
 
-if docker image inspect "$image" &> /dev/null; then
-    echo "Image exists. Removing..."
-    docker rmi "$image"
-else
-    echo "Image does not exist."
-fi
+docker rmi "$image"
